193cbb956fc8349cbc820e63b94607c02498dc47
sailathatammana <32009979+sailathatammana@users.noreply.github.com>
2021-10-10 12:46:09 +0200

    Base setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import HomePage from "./pages/HomePage";
+import MenuPage from "./pages/MenuPage";
+import CategoryPage from "./pages/CategotyPage";
+import ProductPage from "./pages/ProductPage";
+import ContactPage from "./pages/ContactPage";
+import Footer from "./components/Footer";
+
 export default function App() {
   return (
     <div className="App">
-      <h1>Brillo's BBQ</h1>
+      <Router>
+        <NavBar />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/menu" component={MenuPage} />
+          <Route exact path="/category" component={CategoryPage} />
+          <Route exact path="/product" component={ProductPage} />
+          <Route exact path="/contact" component={ContactPage} />
+        </Switch>
+        <Footer />
+      </Router>
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <ul>
+        <li>
+          <a href="https://www.facebook.com/">
+            <i class="fab fa-facebook-square"></i>
+          </a>
+        </li>
+        <li>
+          <a href="https://www.instagram.com/" target="blank">
+            <i class="fab fa-instagram-square"></i>
+          </a>
+        </li>
+      </ul>
+      <p>2021 . Brillo's</p>
+    </footer>
+  );
+}
